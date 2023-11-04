1577df9f9bb8436ee662366ea1765a0fc2d37fb6
Hamza Shemsu <hshemsu10@gmail.com>
2023-11-03 20:06:40 -0400

    install router package

--- crewmates/src/App.jsx
+++ crewmates/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {createClient} from '@supabase/supabase-js'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
 
 const supabaseUrl = 'https://smywirsxepshhyxgcdzs.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNteXdpcnN4ZXBzaGh5eGdjZHpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg4OTQzMDUsImV4cCI6MjAxNDQ3MDMwNX0.5ZIAAbJo-rwNJYINoE4mnTFCH5DKnMztqc4mZZ4dpKo';
@@ -12,18 +14,22 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 //still need to create a table
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
       <h1>Hamza's Football Club</h1>
       <h3>Create your footballer and join the squad!</h3>
 
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-
-
-
+          <Route path="/create" element={<Create />} />
+          
+          <Route path="/update" element={<Update />} />
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
